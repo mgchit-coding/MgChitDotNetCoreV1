06ebbe27f7dd8c56b824ce09ae859d39f878bf01
mgchit <chitlunh@gmail.com>
2024-04-22 23:19:20 +0630

    [InProgress] Dapper

---
+++ MgChitDotNetCore.Shared/Services/DapperService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MgChitDotNetCore.Shared.Services
+{
+    public class DapperService
+    {
+        public string GetConnection()
+        {
+            var connection = new SqlConnectionStringBuilder()
+            {
+                DataSource = ".",
+                InitialCatalog = "TestDb",
+                UserID = "sa",
+                Password = "sasa@123",
+            };
+            return connection.ConnectionString;
+        }
+    }
+}
