d4990bcbc39ef12d9047cd59285e146189e3e7e6
mgchit <chitlunh@gmail.com>
2024-05-30 01:29:08 +0630

    -

---
+++ MgChitDotNetCore.Model/Models/BlogViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MgChitDotNetCore.Model.Models;
+
+public class BlogViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Content { get; set; }
+}
